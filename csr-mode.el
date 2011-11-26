e9b4ac056b1cb51aed7e12fa94ecdc38137b3902
Gregory B. Brown <gb@trailbound.net>
2011-11-26 15:29:12 -0500

    Font lock highlighting cleaned up and working. Simple indentation based on brackets implemented. csr-mode is functional enough for use.

--- csr-mode.el
+++ csr-mode.el
@@ -24,7 +24,7 @@
 ;;; Commentary:
 ;;
 ;; Thanks to Scott Andrew Borton for his tutorial and example on
-;; Emacs mode creation. CSR-Mode was built based on and borrowing
+;; Emacs mode creation. CSR-Mode was built based on and stealing
 ;; heavily from his wpdl-mode example.
 
 
@@ -39,48 +39,55 @@
 (add-to-list 'auto-mode-alist '("\\.csr\\'" . csr-mode))
 
 
-;;(defconst csr-builtin-list
-;;  '("addressmap" "group" "register" "field" "port" "memory")
-;;  "List of CSR objects.")
+(defconst csr-object-list
+  '("addressmap" "group" "register" "field" "port" "memory" "component")
+  "List of CSR objects.")
+
+(defconst csr-control-list
+  '("if" "else" "for" "case")
+  "List of CSR control statements.")
 
 (defconst csr-type-list
   '("enum" "userdefined" "bool" "integer" "number" "string" "reference" "var")
   "List of CSR types.")
 
-(defconst csr-font-lock-keywords
+
+(defconst csr-font-lock-keywords-1
   (list
-   (list (concat "\\<" (regexp-opt csr-type-list t) "\\>") 1 'font-lock-type-face)
-   '("\\<\\(aressmap\\|field\\|register\\)\\>" 1 font-lock-keyword-face)
+   (list (concat "\\<" (regexp-opt csr-object-list t) "\\>") 1 'font-lock-function-name-face)
    '("\\('\\w*'\\)" 1 font-lock-variable-name-face)
    )
   "Minimal highlighting expressions for CSR mode.")
 
-;;
-;;   (list (concat "\\_<" (regexp-opt csr-builtin-list t) "\\_>") 1 'font-lock-builtin-face)
-;;   (list (concat "\\_<" (regexp-opt csr-type-list t) "\\_>") 2 'font-lock-type-face)))
 
-;;   (list (concat "\\<" (regexp-opt csr-builtin-list t) "\\>") 1 'font-lock-builtin-face)
-;;  '("\\<\\(addressmap\\|field\\|register\\)\\>" 1 font-lock-keyword-face)
-;;  '("\\('\\w*'\\)" 1 font-lock-variable-name-face))
+(defconst csr-font-lock-keywords-2
+  (append csr-font-lock-keywords-1
+          (list
+           (list (concat "\\<" (regexp-opt csr-type-list t) "\\>") 1 'font-lock-type-face)
+           ))
+  "Additional Keywords to highlight in CSR mode.")
 
-;;(defconst csr-font-lock-keywords-2
-;;  (append csr-font-lock-keywords-1
-;;          (list
-;;           (list (concat "\\<" (regexp-opt csr-type-list t) "\\>") 2 'font-lock-type-face)))
-;;  "Additional Keywords to highlight in CSR mode.")
+(defconst csr-font-lock-keywords-3
+  (append csr-font-lock-keywords-2
+		  (list
+       ;; Highlights address specifications (example: [0x2000])
+       '("\\(\\[[0-9x]+\\]\\)" 1 font-lock-constant-face)
+       ))
+  "Full highlighting in CSR mode.")
 
-;;(defconst csr-font-lock-keywords-3
-;;  (append csr-font-lock-keywords-2
-;;		  (list
-;;		 ; These are some possible built-in values for CSR attributes
-;;			 ; "ROLE" "ORGANISATIONAL_UNIT" "STRING" "REFERENCE" "AND"
-;;			 ; "XOR" "WORKFLOW" "SYNCHR" "NO" "APPLICATIONS" "BOOLEAN"
-;;							 ; "INTEGER" "HUMAN" "UNDER_REVISION" "OR"
-;;		   '("\\<\\(A\\(ND\\|PPLICATIONS\\)\\|BOOLEAN\\|HUMAN\\|INTEGER\\|NO\\|OR\\(GANISATIONAL_UNIT\\)?\\|R\\(EFERENCE\\|OLE\\)\\|S\\(TRING\\|YNCHR\\)\\|UNDER_REVISION\\|WORKFLOW\\|XOR\\)\\>" . font-lock-constant-face)))
-;;  "Balls-out highlighting in CSR mode.")
+;; This is just a placeholder copied from tcl-mode as the
+;; setting of font-lock-defaults was copied from that mode
+;; as well. I'm not entirely sure what the syntactic function's
+;; purpose is, so I need to figure that out in the future and
+;; replace this with whatever makes sense.
+(defvar csr-font-lock-syntactic-keywords
+  ;; Mark the few `#' that are not comment-markers.
+  '(("[^;[{ \t\n][ \t]*\\(#\\)" (1 ".")))
+  "Syntactic keywords for csr-mode.")
 
-;;(defvar csr-font-lock-keywords csr-font-lock-keywords-1
-;;  "Default highlighting expressions for CSRCompiler mode.")
+(defvar csr-font-lock-keywords csr-font-lock-keywords-3
+  "Default highlighting expressions for CSRCompiler mode.")
+
 
 (defun csr-indent-line ()
   "Indent current line as CSRCompiler code."
@@ -89,7 +96,7 @@
   (if (bobp)
 	  (indent-line-to 0)		   ; First line is always non-indented
 	(let ((not-indented t) cur-indent)
-	  (if (looking-at "^[ \t]*END_") ; If the line we are looking at is the end of a block, then decrease the indentation
+	  (if (looking-at ".*[ \t]*}") ; If the line we are looking at is the end of a block, then decrease the indentation
 		  (progn
 			(save-excursion
 			  (forward-line -1)
@@ -99,11 +106,11 @@
 		(save-excursion
 		  (while not-indented ; Iterate backwards until we find an indentation hint
 			(forward-line -1)
-			(if (looking-at "^[ \t]*END_") ; This hint indicates that we need to indent at the level of the END_ token
+			(if (looking-at ".*[ \t]*}") ; This hint indicates that we need to indent at the level of the } token
 				(progn
 				  (setq cur-indent (current-indentation))
 				  (setq not-indented nil))
-			  (if (looking-at "^[ \t]*\\(PARTICIPANT\\|MODEL\\|APPLICATION\\|WORKFLOW\\|ACTIVITY\\|DATA\\|TOOL_LIST\\|TRANSITION\\)") ; This hint indicates that we need to indent an extra level
+			  (if (looking-at ".*[ \t]*{") ; This hint indicates that we need to indent an extra level
 				  (progn
 					(setq cur-indent (+ (current-indentation) default-tab-width)) ; Do the actual indenting
 					(setq not-indented nil))
@@ -112,6 +119,7 @@
 	  (if cur-indent
 		  (indent-line-to cur-indent)
 		(indent-line-to 0))))) ; If we didn't see an indentation hint, then allow no indentation
+
 
 (defvar csr-mode-syntax-table
   (let ((csr-mode-syntax-table (make-syntax-table)))
@@ -132,7 +140,14 @@
   (use-local-map csr-mode-map)
   (set-syntax-table csr-mode-syntax-table)
   ;; Set up font-lock
-  (set (make-local-variable 'font-lock-defaults) '(csr-font-lock-keywords))
+  (set (make-local-variable 'font-lock-defaults)
+       '(csr-font-lock-keywords nil nil nil beginning-of-defun
+ 	 (font-lock-syntactic-keywords . csr-font-lock-syntactic-keywords)
+ 	 (parse-sexp-lookup-properties . t)))
+
+;; The old setting of font-lock-defaults from wpdl-mode tutorial
+;;  (set (make-local-variable 'font-lock-defaults) '(csr-font-lock-keywords))
+
   ;; Register our indentation function
   (set (make-local-variable 'indent-line-function) 'csr-indent-line)
   (setq major-mode 'csr-mode)
